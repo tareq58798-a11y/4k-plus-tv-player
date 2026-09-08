1be4ba3cd3c254bf0601229819b4f84a650b1fd5
tareq58798-a11y <tareq58798@gmail.com>
2026-09-09 02:25:12 +0400

    Correct Android build configuration

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,11 +18,6 @@ android {
 
     buildFeatures { compose = true }
 
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-
     packaging {
         resources.excludes += "/META-INF/{AL2.0,LGPL2.1}"
     }
