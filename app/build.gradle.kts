3bf42ffe34089b013fa69f5898a886dee080c436
tareq58798-a11y <tareq58798@gmail.com>
2026-09-10 12:48:24 +0400

    Set version 0.11.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.fourkplus.tvplayer"
         minSdk = 26
         targetSdk = 35
-        versionCode = 32
-        versionName = "0.11.3"
+        versionCode = 33
+        versionName = "0.11.4"
     }
 
     buildFeatures { compose = true }
