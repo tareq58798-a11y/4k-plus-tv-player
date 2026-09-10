2e503727fe6de229ebbc7949d73ac2ff3a76b386
tareq58798-a11y <tareq58798@gmail.com>
2026-09-10 17:36:54 +0400

    Bump version to 0.13.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.fourkplus.tvplayer"
         minSdk = 26
         targetSdk = 35
-        versionCode = 39
-        versionName = "0.13.0"
+        versionCode = 40
+        versionName = "0.13.1"
     }
 
     buildFeatures { compose = true }
