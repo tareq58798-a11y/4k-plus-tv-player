c478dd278dc830b9bed7c9beb6095dbe9e9a8b3e
tareq58798-a11y <tareq58798@gmail.com>
2026-09-11 17:14:01 +0400

    Restore M3U URL import (0.13.10) (#20)
    * Restore M3U URL import alongside provider login
    
    * Restore M3U URL import alongside provider login
    
    * Restore M3U URL import alongside provider login

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.fourkplus.tvplayer"
         minSdk = 26
         targetSdk = 35
-        versionCode = 48
-        versionName = "0.13.9"
+        versionCode = 49
+        versionName = "0.13.10"
     }
 
     buildFeatures { compose = true }
