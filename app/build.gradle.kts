8dc503b0fd5989f26eaa104c26cc94037cdcd19e
tareq58798-a11y <tareq58798@gmail.com>
2026-09-11 15:30:36 +0400

    Restore Provider Login and remove URL entry (v0.13.6)
    * Restore playlist repository from v0.13.2
    
    * Set version 0.13.6
    
    * Set version 0.13.6
    
    * Remove M3U URL entry and use Provider Login only

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.fourkplus.tvplayer"
         minSdk = 26
         targetSdk = 35
-        versionCode = 44
-        versionName = "0.13.5"
+        versionCode = 45
+        versionName = "0.13.6"
     }
 
     buildFeatures { compose = true }
