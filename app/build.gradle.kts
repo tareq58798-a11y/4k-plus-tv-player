057e66b5a92fada29120c39dfa33ed7e9a667a6a
tareq58798-a11y <tareq58798@gmail.com>
2026-09-12 11:30:23 +0400

    Bump appearance update version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,8 +16,8 @@ android {
         applicationId = "com.fourkplus.tvplayer"
         minSdk = 26
         targetSdk = 35
-        versionCode = 55
-        versionName = "0.15.3"
+        versionCode = 56
+        versionName = "0.15.4"
     }
 
     buildFeatures { compose = true }
